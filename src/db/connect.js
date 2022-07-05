2bd3223e788d6d8dce2192d9bdad315bc0baa2ff
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-05 13:29:03 +0600

    server created

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,2 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWWORD}@curdoperation.qbqwp.mongodb.net/?retryWrites=true&w=majority`
+).then(()=> {
+    console.log('hello')
+})

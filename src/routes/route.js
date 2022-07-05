637d7e98c2cf011f5798cebfd522666cddf233d1
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-06 00:21:47 +0600

    all api created

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,10 +5,10 @@ const UsersCollection = require("../models/user");
 // GET All Users
 router.get("/users", async (req, res) => {
   try {
-    const newUser = await UsersCollection.find();
-    res.status(200).json(newUser);
+    const allUsers = await UsersCollection.find();
+    res.status(200).json(allUsers);
   } catch (err) {
-    console.log(err);
+    res.status(400).json(err)
   }
 });
 
@@ -16,10 +16,10 @@ router.get("/users", async (req, res) => {
 router.get("/users/:id", async (req, res) => {
   try {
     const _id = req.params.id
-    const newUser = await UsersCollection.findById(_id);
-    res.status(200).json(newUser);
+    const singleUser = await UsersCollection.findById(_id);
+    res.status(200).json(singleUser);
   } catch (err) {
-    console.log(err);
+    res.status(400).json(err)
   }
 });
 
@@ -29,6 +29,28 @@ router.post("/users", async (req, res) => {
     const newUser = new UsersCollection(req.body);
     const userCreated = await newUser.save();
     res.status(200).json(userCreated);
+  } catch (err) {
+   res.status(400).json(err)
+  }
+});
+
+// PATCH a User
+router.patch("/users/:id", async (req, res) => {
+  try {
+    const _id = req.params.id
+    const userUpdated = await UsersCollection.findByIdAndUpdate(_id, req.body, {new: true});
+    res.status(200).json(userUpdated);
+  } catch (err) {
+   res.status(400).json(err)
+  }
+});
+
+// DELETE a User
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const _id = req.params.id
+    const deletedUser = await UsersCollection.findByIdAndDelete(_id);
+    res.status(200).json(deletedUser);
   } catch (err) {
    res.status(400).json(err)
   }
